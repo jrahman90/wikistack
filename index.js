315f0e06f0bd1b3a74034771863694bd5138c44a
Lane Zimmerman <lane.zimmerman@gmail.com>
2021-11-18 15:30:56 -0500

    Intro & Setup complete

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const morgan = require("morgan");
 const express = require("express");
 
+//just added this.
+const main = require("./views/main");
+
 
 let app = express();
 
@@ -9,5 +12,13 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  console.log('hello world');
-})
+  let emptyString = 'Here is the test item';
+  res.send(main(emptyString));
+  //console.log('hello mars');
+});
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
